ede99f4a654a492a81381e074d01f6deff39982d
mahmoud77732 <mm8871918@gmail.com>
2025-08-10 16:24:28 +0300

    Practice on JS function types

---
+++ js-functions-05/main.js
@@ -0,0 +1,61 @@
+
+/*
+* Functions
+*/
+
+// Simple function without params
+/*
+function greet() {
+  console.log("Hello!");
+}
+greet(); // call the function
+*/
+
+
+// function with 1 param
+/*
+function greet(name){
+    console.log(`Hello ${name}!`);
+}
+greet("Mahmoud");
+*/
+
+
+// function with// 2 params
+/*
+function addTwoNums(a, b){
+    console.log(`sum of ${a} and ${b} equal to ${a+b}`);
+}
+addTwoNums(10, 5);
+*/
+
+
+// Arrow function 1
+/*
+const multiply = (a, b) => {
+  return a * b;
+};
+console.log(multiply(4, 5)); // 20
+*/
+
+
+// Arrow function 2
+/*
+const multiply = (a, b) => a *b;
+console.log(multiply(10, 10));
+*/
+
+
+// Default parameter value
+/*
+function greetPerson(name = "stranger") {
+  console.log(`Hi, ${name}!`);
+}
+greetPerson();       // Hi, stranger!
+greetPerson("Ali"); // Hi, Ali!
+*/
+
+// -------------------------------------------
+
+
+
